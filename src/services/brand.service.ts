57869a6ceed9a50bc6381291af9c7009864a528c
VikeTus0201 <letuantu0123@gmail.com>
2025-07-21 17:56:55 +0700

    update: axiosInstance Brand

--- src/services/brand.service.ts
+++ src/services/brand.service.ts
@@ -1,10 +1,15 @@
-import axios from 'axios';
+import axiosInstance from '../utils/axiosInstance';
 
 const API_URL = 'http://localhost:8080/api';
 
 export const brandService = {
   getAllBrands: async () => {
-    const res = await axios.get(`${API_URL}/brand?_page=1&_limit=100`);
-    return res.data;
+    try {
+      const res = await axiosInstance.get(`${API_URL}/brand?_page=1&_limit=100`);
+      return res.data;
+    } catch (error) {
+      console.error('Lỗi khi tải danh sách thương hiệu:', error);
+      throw error;
+    }
   },
 };
