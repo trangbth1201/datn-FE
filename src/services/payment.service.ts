055b837f3dff13b24146356af5a171b6c414292a
VikeTus0201 <letuantu0123@gmail.com>
2025-07-21 17:58:55 +0700

    update: axiosInstance Payment

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -1,12 +1,16 @@
 import axiosInstance from "../utils/axiosInstance";
 
 const createVnpayPaymentUrl = async (orderId: string) => {
-  const response = await axiosInstance.post("/payment/vnpay/create", {
-    orderId,
-  });
-  console.log("VNPAY Payment URL:", response.data);
-
-  return response.data;
+  try {
+    const response = await axiosInstance.post("/payment/vnpay/create", {
+      orderId,
+    });
+    console.log("VNPAY Payment URL:", response.data);
+    return response.data;
+  } catch (error: any) {
+    console.error("Lỗi khi tạo URL thanh toán VNPAY:", error.response?.data?.message || error.message);
+    throw error;
+  }
 };
 
 export const paymentService = {
